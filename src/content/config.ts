cd0d7d3d1d5f24c8a19760ee8660717e60820c13
menteora <menteora@gmail.com>
2025-05-11 21:28:26 +0200

    Update 1 file: config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -55,7 +55,7 @@ const postCollection = defineCollection({
 
     title: z.string(),
     excerpt: z.string().optional(),
-    image: z.string().optional(),
+    image: image(),
 
     category: z.string().optional(),
     tags: z.array(z.string()).optional(),
